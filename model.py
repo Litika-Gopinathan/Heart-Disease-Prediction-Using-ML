677d13bf74cec010fb5e3f5d33c6d42cd788b3fd
Litika-Gopinathan <114471493+Litika-Gopinathan@users.noreply.github.com>
2024-02-25 14:27:25 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,33 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 data = pd.read_csv('C:/Users/Windows/Downloads/heart.csv')
 
 data = data.drop_duplicates()
+
+cate_val = []
+cont_val = []
+
+for col in data.columns:
+    if data[col].nunique() <= 10:
+        cate_val.append(col)
+    else:
+        cont_val.append(col)
+
+cate_val.remove('sex')
+cate_val.remove('fbs')
+cate_val.remove('restecg')
+cate_val.remove('exang')
+cate_val.remove('target')
+
+data = pd.get_dummies(data,columns=cate_val,drop_first=False)
+
+st = StandardScaler()
+data[cont_val] = st.fit_transform(data[cont_val])
 
 X = data.drop(columns='target', axis=1)
 Y = data['target']
